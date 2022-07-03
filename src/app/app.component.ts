829c39c8dd57a4db85c7b1670450cc03ec6e154a
PeggyChou <peggychou0610@gmail.com>
2022-07-03 22:08:51 +0800

    HTTP-1 get nav link text by http get a local json file. P.S. Comment out the importation of HttpClientInMemoryWebApiModule to make this work. It is trying to get it from the InMemoryDataService, instead of the local json file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { SharedService } from './shared-module/services/shared.service';
 import { MessagesComponent } from './messages/messages.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,25 @@ import { MessagesComponent } from './messages/messages.component';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   @ViewChild(MessagesComponent) child: MessagesComponent;
   title = 'Tour of Heroes';
   public heroCount: number;
   public subscription: Subscription;
+  public jsonFromMock: any;
+  objectKeys = Object.keys;
 
-  constructor(private sharedService: SharedService) {
+  constructor(private sharedService: SharedService, private http: HttpClient) {
     this.subscription = this.sharedService.getMessage().subscribe(msg => {
       if (!isNaN(msg)) {
         this.heroCount = +msg;
       }
+    });
+  }
+
+  ngOnInit(): void {
+    this.http.get('./assets/data/dashboard-link.json').subscribe(data => {
+      this.jsonFromMock = data;
     });
   }
 
